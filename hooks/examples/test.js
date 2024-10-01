0fee284492515bcbbed209ffb6c82ff763b4f344
sushobhit-lt <sushobhitd@lambdatest.com>
2024-10-01 16:11:44 +0530

    remove commented code

--- hooks/examples/test.js
+++ hooks/examples/test.js
@@ -10,9 +10,7 @@ const USERNAME = process.env.LT_USERNAME || "username";
 const KEY = process.env.LT_ACCESS_KEY || "accessKey";
 
 // gridUrl: gridUrl can be found at automation dashboard
-//const GRID_HOST = process.env.GRID_HOST || "@hub.sushobhit.dev.lambdatest.io/wd/hub";    //dev
-const GRID_HOST =
-  process.env.GRID_HOST || "@hub.lambdatest.com/wd/hub";    //connect to lambdatest hub
+const GRID_HOST = process.env.GRID_HOST || "@hub.lambdatest.com/wd/hub";    //connect to lambdatest hub
 
 async function searchTextOnGoogle() {
   var keys = process.argv;
